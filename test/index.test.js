c601c3ee682d33dd61949b06419dcf546a8fddd6
notlee <lee.moody@ft.com>
2019-09-16 16:24:14 +0100

    Remove a host of dependencies.
    Remove unused dependencies:
    - @oclif/config
    - @oclif/plugin-help
    
    Remove dev dependency `@oclif/test` and its peer dependecy `chai`.
    Instead write integration tests using mocha, sinon, and proclaim;
    the same tools used in other tests and other Origami projects.
    
    Introduce a dependency on `stdout-stderr` to help with the
    intregration test assertions. `@oclif/test` used this under the
    hood. It's a small project though so lock down the version.

--- test/index.test.js
+++ test/index.test.js
@@ -1,28 +1,41 @@
 'use strict';
 
-const { test, expect } = require('@oclif/test');
 const cmd = require('..');
+const proclaim = require('proclaim');
+const { stdout } = require('stdout-stderr');
 
 describe('origami-bundle-size-cli', () => {
-	// Errors with no arguments when not running in a component directory.
-	test
-		.stderr()
-		.do(() => cmd.run([]))
-		.exit(2)
-		.it('exits with an error with no arguments and not in a component directory');
 
-	// Error with only two version arguments, as a component name is required.
-	test
-		.stderr()
-		.do(() => cmd.run(['v1.0.0', 'v1.0.1']))
-		.exit(2)
-		.it('exits with an error with only two arguments');
+	it('exits with an error with no arguments and not in a component directory', async () => {
+		let errorMessage;
+		try {
+			await cmd.run([]);
+		} catch (error) {
+			errorMessage = error.message;
+		}
 
-	// Finds bundle size difference given a component name and two versions.
-	test
-		.stdout()
-		.do(() => cmd.run(['o-test-component', 'v1.0.33', 'v1.0.34']))
-		.it('shows the bundle size difference given a component name and two versions', ctx => {
-			expect(ctx.stdout).to.contain('No bundle size difference found');
-		});
+		proclaim.include(errorMessage, 'Could not get component details locally');
+	});
+
+	it('error with only two version arguments, as a component name is required', async () => {
+		let errorMessage = '';
+		try {
+			await cmd.run(['v1.0.0', 'v1.0.1']);
+		} catch (error) {
+			errorMessage = error.message;
+		}
+
+		proclaim.include(errorMessage, 'Incorrect number of arguments');
+	});
+
+	it('finds bundle size difference given a component name and two versions', async () => {
+		// start mocking stdout
+		stdout.start();
+		//  run the command
+		await cmd.run(['o-test-component', 'v1.0.33', 'v1.0.34']);
+		// stop mocking stdout
+		stdout.stop();
+		// confirm the message we expect was output
+		proclaim.include(stdout.output, 'No bundle size difference found');
+	});
 });
