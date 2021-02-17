7425532c1558de691dfc8fa07e658d57ccf5bc26
notlee <notlee@users.noreply.github.com>
2021-02-17 15:27:15 +0000

    Remove features which are not supported by v2 components
    - Requires node 12 or later.
    - Removes the ability to compare bundle sizes of any ref pushed to
    Github. It is now only possible to compare the bundle sizes of
    releases. This is because the [Origami Build Service](https://www.ft.com/__origami/service/build/) drops support
    for bundling commit references for components which follow v2 of
    the Origami Component Specification.
    - Requires 3 arguments, the component name and two versions to compare.
    
    closes: https://github.com/Financial-Times/origami-bundle-size-cli/issues/161

--- test/unit/version.test.js
+++ test/unit/version.test.js
@@ -6,7 +6,6 @@ const Version = require('../../src/version');
 const RepoDataClient = require('@financial-times/origami-repo-data-client');
 const execa = require('execa');
 const fs = require('fs');
-const semver = require('semver');
 
 describe('version', () => {
 
@@ -15,7 +14,6 @@ describe('version', () => {
 	let fsReadStub;
 
 	const mockTags = 'v7.1.0\nv7.2.0\naaa\nzzz\n111\n999\n7.2.1\nv7.2.2\nv7.3.0-beta.1\nv7.3.0-beta.2';
-	const mockLatestNonPreReleaseTag = 'v7.2.2';
 	const commitRef = 'ce013625030ba8dba906f756967f9e9ca394464a';
 
 	const mockRepoDataVersion = {
@@ -80,56 +78,6 @@ describe('version', () => {
 				didError = true;
 			}
 			proclaim.isTrue(didError);
-		});
-	});
-
-	describe('createFromLocalDirectory', () => {
-
-		it('errors if given a ref which isn\'t "release", "commit", or a valid semver', async () => {
-			let didError;
-			try {
-				for await (const ref of ['somestring', commitRef, 'v1.5.5.5']) {
-					await Version.createFromLocalDirectory(ref);
-				}
-			} catch (error) {
-				didError = true;
-			}
-			proclaim.isTrue(didError);
-		});
-
-		it('returns a Version for the latest commit in the current directory if given "commit" as a ref', async () => {
-			const version = await Version.createFromLocalDirectory('commit');
-			proclaim.equal(version.ref, commitRef);
-		});
-
-		it('returns a Version for the latest semver tag in the current directory if given "release" as a ref', async () => {
-			const version = await Version.createFromLocalDirectory('release');
-			proclaim.equal(version.ref, semver.valid(mockLatestNonPreReleaseTag));
-		});
-
-		it('returns a Version for the semver tag given if given a semver as a ref', async () => {
-			const version = await Version.createFromLocalDirectory('v4.4.4');
-			proclaim.equal(version.ref, semver.valid('v4.4.4'));
-		});
-
-		it('returns a Version with a name found from origami.json in the current directory ', async () => {
-			const version = await Version.createFromLocalDirectory('v4.4.4');
-			proclaim.equal(version.name, mockBowerManifest.name);
-		});
-
-		it('returns a Version with brands found in the current directory if not given a semver ref', async () => {
-			const version = await Version.createFromLocalDirectory('commit');
-			const brands = version.brands;
-			proclaim.isTrue(repoDataStub.notCalled);
-			proclaim.isTrue(fsReadStub.called);
-			proclaim.deepEqual(brands, mockOrigmaiManifest.brands);
-		});
-
-		it('returns a Version with brands from origami repo data if given a semver ref', async () => {
-			const version = await Version.createFromLocalDirectory('v1.1.1');
-			const brands = version.brands;
-			proclaim.isTrue(repoDataStub.called);
-			proclaim.deepEqual(brands, mockOrigmaiManifest.brands);
 		});
 	});
 });
